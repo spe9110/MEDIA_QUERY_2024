918f501d6cca2a679ce0db205486d512bffe0061
Spencer WAWAKU <spencerm365@outlook.fr>
2024-02-06 23:38:02 +0100

    add the sass tuto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = updateSize;
 
 function updateSize() {
     myWidth = window.innerWidth;
-    document.querySelector(".size").innerHTML = "Width: " + myWidth + "px";
+    document.getElementById("size").innerHTML = "Width: " + myWidth + "px";
 }
 
 
